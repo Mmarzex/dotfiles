ad306d6ec67bfae71241e63785db6eac53248d24
Max Marze <max@marze.dev>
2024-01-19 16:58:39 -0500

    add install.sh

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@ create_symlinks() {
 	# Get the directory in which this script lives.
 	script_dir=$(dirname "$(readlink -f "$0")")
 
-	# Get a list of all files in this directory that start with a dot.
-	files=$(find -maxdepth 1 -type f -name ".*")
+	echo "Config Dir $script_dir"
 
-	# Create a symbolic link to each file in the home directory.
-	for file in $files; do
-		name=$(basename $file)
-		echo "Creating symlink to $name in home directory."
-		rm -rf ~/$name
-		ln -s $script_dir/$name ~/$name
-	done
+	if [ -d ~/.config ]; then
+		for d in $script_dir/.config/*; do
+			echo "Symlinking $d"
+			ln -s $d ~/.config
+		done
+	else
+		ln -s $script_dir/.config ~/
+	fi
 }
 
 create_symlinks
